584b314a6b9e8a17abf22597241bea7e27fa7428
sadilak <s_adilak@hotmail.com>
2021-01-23 20:10:15 +0100

    adjust server.py and index.html

--- server.py
+++ server.py
@@ -34,8 +34,7 @@ def submit_form():
         data = request.form.to_dict()
         write_to_csv(data)
         return redirect('index.html')
-      except Exception as err:
-        print (err)
+      except:
         return 'did not save to database'
     else:
       return 'something went wrong. Try again!'
