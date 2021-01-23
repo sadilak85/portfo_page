79ead8847272bc1750fa08a39491e1b07a9e35f6
sadilak <s_adilak@hotmail.com>
2021-01-24 00:20:23 +0100

    test server.py 4 - flash flask 12

--- server.py
+++ server.py
@@ -32,15 +32,7 @@ def submit_form():
     if request.method == 'POST':
       try:
         data = request.form.to_dict()
-        #write_to_csv(data)
-
-        with open('database.csv', newline='', mode='a') as database2:
-          email = data["email"]
-          subject = data["subject"]
-          message = data["message"]
-          csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-          csv_writer.writerow([email,subject,message])
-
+        write_to_file(data)
         return redirect('index.html')
       except:
         return 'did not save to database'
