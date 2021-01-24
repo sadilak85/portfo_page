5d5340ea4b6895b159d4f2ec815384d622fa2a8d
sadilak <s_adilak@hotmail.com>
2021-01-24 01:01:53 +0100

    test server.py 4 - flash flask 21

--- server.py
+++ server.py
@@ -21,11 +21,12 @@ def write_to_file(data):
 
 def write_to_csv(data):
   with open('database.csv', newline='', mode='a') as database2:
-    email = data["email"]
-    subject = data["subject"]
-    message = data["message"]
-    csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    csv_writer.writerow([email,subject,message])
+    pass
+#    email = data["email"]
+#    subject = data["subject"]
+#    message = data["message"]
+#    csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#    csv_writer.writerow([email,subject,message])
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
