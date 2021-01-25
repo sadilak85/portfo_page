9355221a5b33742e05c1c26001055ec6a45d1e4c
sadilak <s_adilak@hotmail.com>
2021-01-25 12:00:17 +0100

    server.py - flash 23

--- server.py
+++ server.py
@@ -39,10 +39,22 @@ def submit_form():
         data = request.form.to_dict()
         if not write_to_csv(data):
           flash("not a valid email")
-        return "redirect('index.html')"
+        return redirect('index.html')
       except:
         return 'did not save to database'
     else:
       return 'something went wrong. Try again!'
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = Nameform()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+        form = form, name = session.get('name'))
