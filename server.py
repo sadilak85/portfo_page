62afde39e4b4871609233dac79ddfbd601c07a7b
sadilak <s_adilak@hotmail.com>
2021-01-25 12:02:28 +0100

    server.py - flash 24

--- server.py
+++ server.py
@@ -57,4 +57,3 @@ def index():
         form.name.data = ''
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
-        form = form, name = session.get('name'))
