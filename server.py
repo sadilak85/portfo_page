3ab7f7d37a3a090287bbdc7b10d9744d2a32db50
sadilak <s_adilak@hotmail.com>
2021-01-25 13:51:15 +0100

    server.py - flash 35

--- server.py
+++ server.py
@@ -5,47 +5,46 @@ import re
 app=Flask(__name__,template_folder='templates')
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
+app.run(debug = True)
 
 @app.route('/')
 def my_home():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
-    return render_template(page_name)
+  return render_template(page_name)
 
 def write_to_file(data):
+  email = data["email"]
+  message = data["message"]
+  name = data["name"]
   with open('database.txt', mode='a') as database:
-    email = data["email"]
-    message = data["message"]
-    name = data["name"]
     file = database.write(f'\n{email},{message},{name}')
 
 def write_to_csv(data):
+  email = data["email"]
+  message = data["message"]
+  name = data["name"]
+  if not re.fullmatch(r"[^@]+@[^@]+\.[^@]+", email):
+    flash("not a valid email")
+    return
   with open('database.csv', newline='', mode='a') as database2:
-    email = data["email"]
-    message = data["message"]
-    name = data["name"]
-    if not re.fullmatch(r"[^@]+@[^@]+\.[^@]+", email):
-      flash("not a valid email")
-      return
     csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     csv_writer.writerow([email,message,name])
     flash("ok halt")
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    if request.method == 'POST':
-      try:
-        data = request.form.to_dict()
-        write_to_csv(data)
-        return redirect(url_for('submit_form'))
-        #return redirect('index.html')
-      except:
-        return 'did not save to database'
-    else:
-      return 'something went wrong. Try again!'
+  if request.method == 'POST':
+    try:
+      data = request.form.to_dict()
+      write_to_csv(data)
+      return redirect(url_for('submit_form'))
+      #return redirect('index.html')
+    except:
+      return 'did not save to database'
+  else:
+    return 'something went wrong. Try again!'
 
 
-if __name__ == '__main__':
-   app.run(debug = True)
